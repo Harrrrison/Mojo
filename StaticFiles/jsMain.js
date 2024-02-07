3eccac7e369217df56e8cda076fecb52e4f075da
harrrrion <harrisonrigby@icloud.com>
2024-02-07 17:39:09 +0000

    Parsed data to output top artists -- need a front end to output it nicely

--- StaticFiles/jsMain.js
+++ StaticFiles/jsMain.js
@@ -4,6 +4,25 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const accessToken = urlParams.get('access_token');
 
+function findNames(obj) {
+    const names = [];
+
+    function recurse(element) {
+        if (typeof element === 'object' && element !== null) {
+            for (const key in element) {
+                if (key === 'name') {
+                    names.push(element[key]);
+                } else if (typeof element[key] === 'object') {
+                    recurse(element[key]);
+                }
+            }
+        }
+    }
+
+    recurse(obj);
+    return names;
+}
+
 if (accessToken) {
     // Use the access token to make API requests
     fetch('https://api.spotify.com/v1/me', {
@@ -13,14 +32,26 @@ if (accessToken) {
         .then(data => {
             document.getElementById('data').innerHTML = JSON.stringify(data, null, 2);
         })
+
         .catch(error => console.error(error));
 
-    fetch('https://api.spotify.com/v1/me/top/artists', { // I dont think the scope is updated after a login
+    fetch('https://api.spotify.com/v1/me/top/artists?limit=10', { // ERROR incomplete JSON IDK why
         headers: { 'Authorization': 'Bearer ' + accessToken }
     })
         .then(response => response.json())
         .then(data => {
-            document.getElementById('TopTracks').innerHTML = JSON.stringify(data, null, 2);
+            try {
+                const names = findNames(data); // Extract name s
+                // Convert names array to a string for display, e.g., as a list
+                const namesList = names.map(name => `<li>${name}</li>`).join('');
+                document.getElementById('TopTracks').innerHTML = `<ul>${namesList}</ul>`;
+            } catch (e) {
+                console.error("Parsing error:", e);
+                document.getElementById('TopTracks').innerHTML = "Error parsing JSON data.";
+            }
+            //document.getElementById('TopTracks').innerHTML = JSON.stringify(data, null, 2);
         })
         .catch(error => console.error(error));
+
 }
+
