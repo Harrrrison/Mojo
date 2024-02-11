a681afe505b2f177faf525d3ee1f809f6a88402a
harrrrion <harrisonrigby@icloud.com>
2024-02-11 12:08:27 +0000

    Comments (more)

--- StaticFiles/jsMain.js
+++ StaticFiles/jsMain.js
@@ -5,13 +5,17 @@ const urlParams = new URLSearchParams(queryString);
 const accessToken = urlParams.get('access_token');
 
 function findNames(obj) {
+    // This will be modified to find X but work needs to be done
     const names = [];
 
     function recurse(element) {
+        // recursivley itterated though the json as some of the api responses are nested
         if (typeof element === 'object' && element !== null) {
             for (const key in element) {
                 if (key === 'name') {
+                    // keyword name being looked for
                     names.push(element[key]);
+                    // if found the name is pushed to the array, to be returned
                 } else if (typeof element[key] === 'object') {
                     recurse(element[key]);
                 }
@@ -35,13 +39,13 @@ if (accessToken) {
 
         .catch(error => console.error(error));
 
-    fetch('https://api.spotify.com/v1/me/top/artists?limit=10&time_range=short_term', { // ERROR incomplete JSON IDK why
+    fetch('https://api.spotify.com/v1/me/top/artists?limit=10&time_range=short_term', {
         headers: { 'Authorization': 'Bearer ' + accessToken }
     })
         .then(response => response.json())
         .then(data => {
             try {
-                const names = findNames(data); // Extract name s
+                const names = findNames(data); // Extract names
                 // Convert names array to a string for display, e.g., as a list
                 const namesList = names.map(name => `<li>${name}</li>`).join('');
                 document.getElementById('TopTracks').innerHTML = `<ul>${namesList}</ul>`;
@@ -49,7 +53,6 @@ if (accessToken) {
                 console.error("Parsing error:", e);
                 document.getElementById('TopTracks').innerHTML = "Error parsing JSON data.";
             }
-            //document.getElementById('TopTracks').innerHTML = JSON.stringify(data, null, 2);
         })
         .catch(error => console.error(error));
 
