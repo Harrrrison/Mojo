48b2fe349be9c57d7204b44eea836d39d82b0202
harrrrion <harrisonrigby@icloud.com>
2024-02-14 17:12:24 +0000

    Working on making the main page dragable

--- StaticFiles/jsMain.js
+++ StaticFiles/jsMain.js
@@ -49,13 +49,87 @@ if (accessToken) {
                 const names = findNames(data); // Extract names
                 // Convert names array to a string for display, e.g., as a list
                 const namesList = names.map(name => `<li>${name}</li>`).join('');
-                document.getElementById('TopTracks').innerHTML = `<ul>${namesList}</ul>`;
+                document.getElementById('topArtists').innerHTML = `<ul>${namesList}</ul>`;
             } catch (e) {
                 console.error("Parsing error:", e);
-                document.getElementById('TopTracks').innerHTML = "Error parsing JSON data.";
+                document.getElementById('topArtists').innerHTML = "Error parsing JSON data.";
             }
         })
         .catch(error => console.error(error));
 
 }
 
+// Draggable code
+// Below is the commented out method I wrote (doenst work very well if at all)
+//dragElement(document.getElementById("TopArtistsDraggableBox"));
+
+function dragElement(elmnt) {
+
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+
+    if (document.getElementById(elmnt.id + "Header")) {
+        // if present, the header is where you move the DIV from:
+        document.getElementById(elmnt.id + "Header").onmousedown = dragMouseDown;
+    } else {
+        // otherwise, move the DIV from anywhere inside the DIV:
+        elmnt.onmousedown = dragMouseDown;
+    }
+
+
+
+function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+}
+
+
+function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+}
+
+function closeDragElement() {
+    document.onmouseup = null;
+    document.onmousemove = null;
+
+}
+}
+
+//interactjs.io
+
+// es6 import
+import interact from 'interactjs'
+
+const position = { x: 500, y: 800}
+
+interact('.draggable').draggable({
+
+    listeners:{
+        // debugger
+        start(event){
+            console.log('drag started', event);
+        },
+
+        move(event){
+            position.x += event.dx;
+            position.y += event.dy;
+
+            event.target.style.transform = `translate(${position.x}px, ${position.y}px)`;
+        }
+    }
+})
