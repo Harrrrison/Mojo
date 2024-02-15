e40231384b41ffcb78da0ffc6da827ded647c017
harrrrion <harrisonrigby@icloud.com>
2024-02-15 22:30:50 +0000

    resize the box WIP (Grid snap will be implemented soon)

--- StaticFiles/jsMain.js
+++ StaticFiles/jsMain.js
@@ -136,3 +136,39 @@ interact('.draggable').draggable({
         }
     }
 });
+
+interact('.resizable').resizable({
+
+    edges: { left: true, right: true, bottom: true, top: true },
+
+    listeners: {
+      move (event) {
+        var target = event.target
+        var x = (parseFloat(target.getAttribute('data-x')) || 0)
+        var y = (parseFloat(target.getAttribute('data-y')) || 0)
+
+        // update the element's style
+        target.style.width  = event.rect.width + 'px'
+        target.style.height = event.rect.height + 'px'
+
+        // translate when resizing from top or left edges
+        x += event.deltaRect.left
+        y += event.deltaRect.top
+
+        target.style.webkitTransform = target.style.transform =
+          'translate(' + x + 'px,' + y + 'px)'
+
+        target.setAttribute('data-x', x)
+        target.setAttribute('data-y', y)
+      }
+    },
+    modifiers: [
+      interact.modifiers.restrictEdges({
+        outer: 'parent'
+      }),
+      interact.modifiers.restrictSize({
+        min: { width: 100, height: 50 }
+      })
+    ],
+    inertia: true
+  });
