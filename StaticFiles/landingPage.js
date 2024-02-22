ba361f06a88f5d1fda23be852174f5182b41d4fd
harrrrion <harrisonrigby@icloud.com>
2024-02-22 18:35:00 +0000

    now works better for multiple stats

--- StaticFiles/landingPage.js
+++ StaticFiles/landingPage.js
@@ -5,6 +5,6 @@ const accessToken = urlParams.get('access_token');
 
 fetchStats(accessToken, 'https://api.spotify.com/v1/me/top/artists?limit=5&time_range=long_term',
     'name', 'item-container', true);// need the element Id
-fetchStats(accessToken, 'https://api.spotify.com/v1/me/top/tracks?limit=5&time_range=long_term',
-    'followers', 'artist-followers', true);
+//fetchStats(accessToken, 'https://api.spotify.com/v1/me/top/tracks?limit=5&time_range=long_term',
+ //   'followers', 'artist-followers', true);
 
