bc6828143949e7a0a996268e01ecb60edfced0c2
harrrrion <harrisonrigby@icloud.com>
2024-02-23 10:12:09 +0000

    tweaking the main page need to add the classes into each <p> for the fetch stats

--- StaticFiles/landingPage.js
+++ StaticFiles/landingPage.js
@@ -3,6 +3,32 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const accessToken = urlParams.get('access_token');
 
+// I know this is a duplicate block of code but when i use export and import it break the stats page so this is the
+// fix for not
+
+function findData(obj, keyToFind) {
+    // This will be modified to find X but work needs to be done
+    const names = [];
+
+    function recurse(element) {
+        // recursivley itterated though the json as some of the api responses are nested
+        if (typeof element === 'object' && element !== null) {
+            for (const key in element) {
+                if (key === keyToFind) {
+                    // keyword name being looked for
+                    names.push(element[key]);
+                    // if found the name is pushed to the array, to be returned
+                } else if (typeof element[key] === 'object') {
+                    recurse(element[key]);
+                }
+            }
+        }
+    }
+
+    recurse(obj);
+    return names;
+}
+
 function fetchStats(accessToken, url, keyToFind, elementId, multiple = false) {
     fetch(url, {
         headers: { 'Authorization': 'Bearer ' + accessToken }
